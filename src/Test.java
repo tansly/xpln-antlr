af384d659d1db2d848bb07fa126ade9c992fe32a
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-11-16 23:38:47 +0300

    Add a simple test program with a visitor implementation

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,38 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+class EvalVisitor extends xplnBaseVisitor<Double> {
+    @Override
+    public Double visitExprAdd(xplnParser.ExprAddContext ctx) {
+        return visit(ctx.expr()) + visit(ctx.term());
+    }
+
+    @Override
+    public Double visitTermMul(xplnParser.TermMulContext ctx) {
+        return visit(ctx.term()) * visit(ctx.factor());
+    }
+
+    @Override
+    public Double visitFactorNum(xplnParser.FactorNumContext ctx) {
+        return Double.valueOf(ctx.NUM().getText());
+    }
+}
+
 public class Test {
     public static void main(String[] args) throws Exception {
         ANTLRInputStream input = new ANTLRInputStream(System.in);
         xplnLexer lexer = new xplnLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         xplnParser parser = new xplnParser(tokens);
-        ParseTree tree = parser.assign();
+        ParseTree tree = parser.expr();
         System.out.println(tree.toStringTree(parser));
+
+        /*
+        EvalVisitor eval = new EvalVisitor();
+        System.out.println(eval.visit(tree));
+        */
     }
 }
