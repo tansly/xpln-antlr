848a8e86e623ac51a7ca5bc07244eee7273a4625
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-11-22 21:55:36 +0300

    Remove unused imports

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,6 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Test {
     public static void main(String[] args) throws Exception {
         ANTLRInputStream input = new ANTLRInputStream(System.in);
