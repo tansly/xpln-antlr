9d2c33731cf48537c36836b110668b7bcf195aac
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-11-22 22:03:34 +0300

    Improve the main program
    Read input from file
    Fix deprecation warning

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,12 @@ import org.antlr.v4.runtime.tree.*;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
+        if (args.length != 1) {
+            System.err.println("Usage: java Test <filename>");
+            return;
+        }
+
+        CharStream input = CharStreams.fromFileName(args[0]);
         xplnLexer lexer = new xplnLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         xplnParser parser = new xplnParser(tokens);
