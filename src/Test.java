57f4c4b0c7434443d2a23927e6ab74c417174571
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-11-22 22:30:04 +0300

    Exit if there are syntax errors

--- src/Test.java
+++ src/Test.java
@@ -13,9 +13,14 @@ public class Test {
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         xplnParser parser = new xplnParser(tokens);
         ParseTree tree = parser.start();
-        System.out.println(tree.toStringTree(parser));
+        if (parser.getNumberOfSyntaxErrors() != 0) {
+            System.err.println("Program is ill-formed.");
+            return;
+        }
 
+        System.out.println(tree.toStringTree(parser));
         System.out.println();
+
         CheckReturnVisitor checker = new CheckReturnVisitor();
         if (checker.visit(tree)) {
             System.out.println("All functions contain at least one return statement.");
